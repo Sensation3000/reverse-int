40708b379eceda8443590c19118e5f6c1ef7ed26
Sensation3000 <omsksmp@gmail.com>
2024-08-10 22:08:16 +0300

    init: add reverse-int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
+    let arr = ("" + n).split("")
+    if (arr[0] === "-"){
+       return +(arr.slice(1).reverse().join(""))
+    } else {
+       return +(("" + n).split("").reverse().join(""))
+    }
 }
